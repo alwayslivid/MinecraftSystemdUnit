d92646a16e57dc7acc55bddc9cf9b219d708569e
Panagiotis Vasilopoulos <hello@alwayslivid.com>
2021-07-31 16:51:29 +0300

    Bump Minecraft version

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo usermod -a -G minecraft minecraft
 sudo mkdir -p /opt/minecraft/survival
 sudo chown -R minecraft:minecraft /opt/minecraft/survival
 
-wget --output-document=paper.jar https://papermc.io/api/v1/paper/1.16.4/latest/download
+wget --output-document=paper.jar https://papermc.io/api/v1/paper/1.17.1/latest/download
 sudo mv paper.jar /opt/minecraft/survival
 sudo cp minecraft@.service /etc/systemd/system
 sudo systemctl enable --now minecraft@survival
