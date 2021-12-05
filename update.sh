656e6eb143fd43d072475760935b9ed1bd875934
Blumlaut <13604413+Blumlaut@users.noreply.github.com>
2021-12-05 13:47:00 +0100

    fix

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@ mcVersion=1.16.4
 
 latestBuild=$(curl https://papermc.io/api/v2/projects/paper/versions/$mcVersion | jq -r .builds[-1])
 buildBin=$(curl https://papermc.io/api/v2/projects/paper/versions/$mcVersion/builds/$lastBuild | jq -r .downloads.application.name)
-wget --output-document=paper.jar https://papermc.io/api/v2/projects/paper/versions/1.18/builds/$lastBuild/downloads/$buildBin
+wget --output-document=paper.jar https://papermc.io/api/v2/projects/paper/versions/$mcVersion/builds/$lastBuild/downloads/$buildBin
 
 sudo mv paper.jar /opt/minecraft/survival
 sudo systemctl restart minecraft@survival
